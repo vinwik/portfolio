43d509c40cbe3dd2f75c6b43aa37febb497cb842
vinwik <vinwik@hotmail.com>
2020-02-08 02:28:45 +0000

    test serviceWorker

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
-// import * as serviceWorker from "./serviceWorker";
+import * as serviceWorker from "./components/serviceWorker";
 
 ReactDOM.render(<App />, document.getElementById("root"));
