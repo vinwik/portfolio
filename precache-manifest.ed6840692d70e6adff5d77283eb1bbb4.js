4d4f0fdbb25653c3b499922aedb29cb8613283eb
vinwik <vinwik@hotmail.com>
2020-02-08 00:09:12 +0000

    Updates

---
+++ precache-manifest.ed6840692d70e6adff5d77283eb1bbb4.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5306d229bd6988238e7b1118b7c49b14",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "9b473eec6e524cb30e45",
+    "url": "/portfolio/static/css/main.863387ec.chunk.css"
+  },
+  {
+    "revision": "8d68aa7b6e9f74e8a9c1",
+    "url": "/portfolio/static/js/2.3a2f2e9c.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/portfolio/static/js/2.3a2f2e9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b473eec6e524cb30e45",
+    "url": "/portfolio/static/js/main.e1d28690.chunk.js"
+  },
+  {
+    "revision": "0d98471f6c3b69ca4251",
+    "url": "/portfolio/static/js/runtime-main.44087e9b.js"
+  },
+  {
+    "revision": "4a0aa59c887ca20a42f8bb21b4fd4dc2",
+    "url": "/portfolio/static/media/angle-down.4a0aa59c.svg"
+  },
+  {
+    "revision": "b0817084e4de102b479bf1ec8a247a68",
+    "url": "/portfolio/static/media/angle-up.b0817084.svg"
+  },
+  {
+    "revision": "7eb8e9c3067d78f4a1004bb757e7349b",
+    "url": "/portfolio/static/media/bg.7eb8e9c3.jpg"
+  },
+  {
+    "revision": "2164c928f9f3c154430ef6024b2293a1",
+    "url": "/portfolio/static/media/codepen.2164c928.svg"
+  },
+  {
+    "revision": "39c094f9f51fb898078a09cdea6e05b4",
+    "url": "/portfolio/static/media/envelope.39c094f9.svg"
+  },
+  {
+    "revision": "0b2869a0e871603737ec41fe6a799c35",
+    "url": "/portfolio/static/media/eye.0b2869a0.svg"
+  },
+  {
+    "revision": "f5a069d24c7855d24188c14f570b6a57",
+    "url": "/portfolio/static/media/github.f5a069d2.svg"
+  },
+  {
+    "revision": "22bd17393aa39a4cfec33c206db5f0df",
+    "url": "/portfolio/static/media/linkedin-in.22bd1739.svg"
+  },
+  {
+    "revision": "4e1194ab19258e66063e70792e6fe367",
+    "url": "/portfolio/static/media/logo.4e1194ab.svg"
+  },
+  {
+    "revision": "43ba411782e729aba160d52be718c13a",
+    "url": "/portfolio/static/media/profile.43ba4117.jpg"
+  },
+  {
+    "revision": "9a11e9b9542846fd1d519d25279ca311",
+    "url": "/portfolio/static/media/weather-app.9a11e9b9.jpg"
+  }
+]);
