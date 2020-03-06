d0699ff3a7552ac1b7f85c5739b81c382e481690
vinwik <vinwik@hotmail.com>
2020-03-06 01:29:51 +0000

    mobile 100vh fixed with hook useWindowHeight

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -1,14 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { ReactComponent as Angle } from "./angle-down.svg";
 import "./Landing.css";
 import Social from "../Social/Social";
 
 const Landing = () => {
+  const getWindowHeight = () => {
+    const vh = window.innerHeight;
+    return vh;
+  };
+
+  const useWindowHeight = () => {
+    const [windowHeight, setWindowHeight] = useState(getWindowHeight());
+    useEffect(() => {
+      function handleResize() {
+        setWindowHeight(getWindowHeight());
+      }
+      window.addEventListener("resize", handleResize);
+      return () => {
+        window.removeEventListener("resize", handleResize);
+      };
+    }, []);
+    return windowHeight;
+  };
+
+  const vh = useWindowHeight();
+
   return (
-    <main id="landing">
+    <main id="landing" style={{ height: `${vh}px` }}>
       <div className="bg">
         <div className="wrapper">
-          <p> Hi, I'm</p>
+          <p>Hi, I'm</p>
           <h1>
             <span>Vincent Wikart</span>,
           </h1>
