685a58f2d8557a5d28cf087096e2c5bde6f7ba84
vinwik <vinwik@hotmail.com>
2020-03-06 14:42:52 +0000

    fade-in on refresh

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,17 +8,21 @@ import Contact from "./Contact/Contact";
 import Footer from "./Footer/Footer";
 import "./App.css";
 
+import Fade from "react-reveal/Fade";
+
 class App extends Component {
   render() {
     return (
       <React.Fragment>
         <Header />
-        <Landing />
-        <About />
-        <Skillset />
-        <Projects />
-        <Contact />
-        <Footer />
+        <Fade>
+          <Landing />
+          <About />
+          <Skillset />
+          <Projects />
+          <Contact />
+          <Footer />
+        </Fade>
       </React.Fragment>
     );
   }
--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -28,30 +28,28 @@ const Landing = () => {
   const vh = useWindowHeight();
 
   return (
-    <Fade>
-      <main id="landing" style={{ height: `${vh}px` }}>
-        <div className="bg">
-          <div className="wrapper">
-            <Fade left>
-              <p>Hi, I'm</p>
-              <h1>
-                <span>Vincent Wikart</span>,
-              </h1>
-              <h2>Creative Front End Developer.</h2>
-            </Fade>
-            <Fade bottom delay={700}>
-              <a href="#projects" className="btn btn-accent">
-                Jump to projects
-              </a>
-            </Fade>
-            <Social className="icon" fade="1200" />
-          </div>
-          <a href="#about" className="angle-down" aria-label="Go to About">
-            <Angle alt="About me" className="svg" />
-          </a>
+    <main id="landing" style={{ height: `${vh}px` }}>
+      <div className="bg">
+        <div className="wrapper">
+          <Fade left>
+            <p>Hi, I'm</p>
+            <h1>
+              <span>Vincent Wikart</span>,
+            </h1>
+            <h2>Creative Front End Developer.</h2>
+          </Fade>
+          <Fade bottom delay={700}>
+            <a href="#projects" className="btn btn-accent">
+              Jump to projects
+            </a>
+          </Fade>
+          <Social className="icon" fade="1200" />
         </div>
-      </main>
-    </Fade>
+        <a href="#about" className="angle-down" aria-label="Go to About">
+          <Angle alt="About me" className="svg" />
+        </a>
+      </div>
+    </main>
   );
 };
 
