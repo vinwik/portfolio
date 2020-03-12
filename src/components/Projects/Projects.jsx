2ceda8d102acfdb7f5fc6d8e6fdd3f82f5bd8d57
vinwik <vinwik@hotmail.com>
2020-03-12 02:32:53 +0000

    simple fade-in on projects

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -8,6 +8,7 @@ import LiveBtn from "./LiveBtn/LiveBtn";
 import LivePreview from "./LivePreview/LivePreview";
 
 import ScrollableAnchor from "react-scrollable-anchor";
+import Fade from "react-reveal/Fade";
 
 const Projects = () => {
   return (
@@ -19,52 +20,54 @@ const Projects = () => {
             const { title, description, infoList, url, repo, img } = project;
 
             return (
-              <div className="content" key={title}>
-                <div className="description">
-                  <h3>{title}</h3>
-                  <p>{description}</p>
-                  <ul>
-                    {infoList.map(li => {
-                      const { info } = li;
-                      return <li key={info}>{info}</li>;
-                    })}
-                  </ul>
-                  <div>
-                    <a href={url} target="_blank" rel="noopener noreferrer">
-                      <LiveBtn />
-                    </a>
-                    {// will not show source code link if no repo
-                    repo && (
-                      <a
-                        href={repo}
-                        className="link"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                      >
-                        Source Code
+              <Fade>
+                <div className="content" key={title}>
+                  <div className="description">
+                    <h3>{title}</h3>
+                    <p>{description}</p>
+                    <ul>
+                      {infoList.map(li => {
+                        const { info } = li;
+                        return <li key={info}>{info}</li>;
+                      })}
+                    </ul>
+                    <div>
+                      <a href={url} target="_blank" rel="noopener noreferrer">
+                        <LiveBtn />
                       </a>
-                    )}
+                      {// will not show source code link if no repo
+                      repo && (
+                        <a
+                          href={repo}
+                          className="link"
+                          target="_blank"
+                          rel="noopener noreferrer"
+                        >
+                          Source Code
+                        </a>
+                      )}
+                    </div>
                   </div>
-                </div>
 
-                <a
-                  href={url}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="screenshot"
-                >
-                  <img
-                    className="img-fluid"
-                    src={img}
-                    alt="weather-app"
+                  <a
+                    href={url}
                     target="_blank"
-                    rel="noreferrer"
-                  />
-                  <h3 className="inner-text">
-                    <LivePreview />
-                  </h3>
-                </a>
-              </div>
+                    rel="noopener noreferrer"
+                    className="screenshot"
+                  >
+                    <img
+                      className="img-fluid"
+                      src={img}
+                      alt="weather-app"
+                      target="_blank"
+                      rel="noreferrer"
+                    />
+                    <h3 className="inner-text">
+                      <LivePreview />
+                    </h3>
+                  </a>
+                </div>
+              </Fade>
             );
           })}
         </div>
