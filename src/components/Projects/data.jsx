a83f8f5b92d37dfe3fa1a46537027bd73da8bd33
vinwik <vinwik@hotmail.com>
2020-04-05 16:58:55 +0100

    added devchan in project section

--- src/components/Projects/data.jsx
+++ src/components/Projects/data.jsx
@@ -1,17 +1,29 @@
 export const projectsData = [
   {
+    title: "devChan",
+    description: "devChan is a social network for developers",
+    infoList: [
+      { info: "MERN Stack" },
+      { info: "Mongo Atlas" },
+      { info: "Github integration" },
+      { info: "JSON Web Token" },
+    ],
+    url: "https://devchan.herokuapp.com/",
+    repo: "https://github.com/vinwik/devchan",
+    img: "img/devchan.jpg",
+  },
+  {
     title: "Mealize",
     description:
       "Organize your meals with Mealize, the recipe App you need for your meal prep. You also can add ingredients to your Cart directly from your favourite recipes, which make Mealize the perfect food shopping companion",
     infoList: [
-      { info: "React Native" },
-      { info: "Styled Components" },
-      { info: "MongoDB" },
-      { info: "Spoonacular API" }
+      { info: "React" },
+      { info: "Context API" },
+      { info: "Spoonacular API" },
     ],
     url: "https://vinwik.github.io/mealize/",
     repo: "https://github.com/vinwik/mealize",
-    img: "img/mealize.jpg"
+    img: "img/mealize.jpg",
   },
   {
     title: "Penny'Shop",
@@ -20,11 +32,11 @@ export const projectsData = [
     infoList: [
       { info: "React JS" },
       { info: "Context API" },
-      { info: "Paypal Sandbox" }
+      { info: "Paypal Sandbox" },
     ],
     url: "https://pennyshop.netlify.com/",
     repo: "https://github.com/vinwik/pennyshop",
-    img: "img/pennyshop.jpg"
+    img: "img/pennyshop.jpg",
   },
   {
     title: "Weather App",
@@ -33,10 +45,10 @@ export const projectsData = [
     infoList: [
       { info: "Vanilla JS" },
       { info: "ES8 Async/Await" },
-      { info: "OpenWeather API" }
+      { info: "OpenWeather API" },
     ],
     url: "https://vinwik.github.io/weather-app/",
     repo: "https://github.com/vinwik/weather-app",
-    img: "img/weather-app.jpg"
-  }
+    img: "img/weather-app.jpg",
+  },
 ];
