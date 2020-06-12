2868f38a2333994fe197f6056651e004a5150908
Vincent Wikart <vinwik@hotmail.com>
2020-06-12 19:53:12 +0100

    Update projects

--- src/components/Projects/data.jsx
+++ src/components/Projects/data.jsx
@@ -1,5 +1,32 @@
 export const projectsData = [
   {
+    title: "Mealize",
+    description:
+      "Organize your meals with Mealize, the recipe App you need for your meal prep. You also can add ingredients to your Cart directly from your favourite recipes, which make Mealize the perfect food shopping companion",
+    infoList: [
+      { info: "React Native" },
+      { info: "Redux" },
+      { info: "Firebase" },
+      { info: "Spoonacular API" },
+    ],
+    url:
+      "https://appetize.io/app/7fzwxm7gb6gxa4jgm2r4jp7nag?device=pixel4&scale=75&orientation=portrait&osVersion=10.0&deviceColor=black&autoplay=true",
+    repo: "https://github.com/vinwik/mealize-native",
+    img: "img/mealize-native.jpg",
+  },
+  {
+    title: "Moovidia",
+    description: "The Netflix UI experiment",
+    infoList: [
+      { info: "React JS" },
+      { info: "useContext" },
+      { info: "RequestAnimationFrame" },
+    ],
+    url: "https://moovidia.netlify.com/",
+    repo: "https://github.com/vinwik/moovidia",
+    img: "img/moovidia.jpg",
+  },
+  {
     title: "devChan",
     description: "devChan is a social network for developers",
     infoList: [
@@ -12,19 +39,19 @@ export const projectsData = [
     repo: "https://github.com/vinwik/devchan",
     img: "img/devchan.jpg",
   },
-  {
-    title: "Mealize",
-    description:
-      "Organize your meals with Mealize, the recipe App you need for your meal prep. You also can add ingredients to your Cart directly from your favourite recipes, which make Mealize the perfect food shopping companion",
-    infoList: [
-      { info: "React" },
-      { info: "Context API" },
-      { info: "Spoonacular API" },
-    ],
-    url: "https://vinwik.github.io/mealize/",
-    repo: "https://github.com/vinwik/mealize",
-    img: "img/mealize.jpg",
-  },
+  // {
+  //   title: "Mealize",
+  //   description:
+  //     "Organize your meals with Mealize, the recipe App you need for your meal prep. You also can add ingredients to your Cart directly from your favourite recipes, which make Mealize the perfect food shopping companion",
+  //   infoList: [
+  //     { info: "React" },
+  //     { info: "Context API" },
+  //     { info: "Spoonacular API" },
+  //   ],
+  //   url: "https://vinwik.github.io/mealize/",
+  //   repo: "https://github.com/vinwik/mealize",
+  //   img: "img/mealize.jpg",
+  // },
   {
     title: "Penny'Shop",
     description:
@@ -38,17 +65,18 @@ export const projectsData = [
     repo: "https://github.com/vinwik/pennyshop",
     img: "img/pennyshop.jpg",
   },
-  {
-    title: "Weather App",
-    description:
-      "Nice Home page with weather real time weather, access to my main Social Apps, and Search bar for Google and YouTube",
-    infoList: [
-      { info: "Vanilla JS" },
-      { info: "ES8 Async/Await" },
-      { info: "OpenWeather API" },
-    ],
-    url: "https://vinwik.github.io/weather-app/",
-    repo: "https://github.com/vinwik/weather-app",
-    img: "img/weather-app.jpg",
-  },
+
+  // {
+  //   title: "Weather App",
+  //   description:
+  //     "Nice Home page with weather real time weather, access to my main Social Apps, and Search bar for Google and YouTube",
+  //   infoList: [
+  //     { info: "Vanilla JS" },
+  //     { info: "ES8 Async/Await" },
+  //     { info: "OpenWeather API" },
+  //   ],
+  //   url: "https://vinwik.github.io/weather-app/",
+  //   repo: "https://github.com/vinwik/weather-app",
+  //   img: "img/weather-app.jpg",
+  // },
 ];
